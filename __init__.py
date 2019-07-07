1d148c217624f47b5b2a0ca8f49c32934728ed11
Alexandre Barrozo <barrozo@tutanota.com>
2019-07-07 21:56:36 +0200

    Include image classification notebook for flowers.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from .mllib import MLDataset, CombinedAttributesAdder, load_data, model_loader, model_saver
-from .mycallbacks import LRFinder, SGDRScheduler
+from mllib import MLDataset, CombinedAttributesAdder, load_data, model_loader, model_saver, mycallbacks
+#from .mycallbacks import LRFinder, SGDRScheduler
